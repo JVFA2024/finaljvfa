c5155a2d17320930765241471433140e8f2af0e1
JVFA2024 <165958658+JVFA2024@users.noreply.github.com>
2024-05-25 23:35:28 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-  #baseURL: "http://localhost:3000",
   baseURL: "https://api-rwqk.onrender.com/",
   headers: {
     "Content-Type": "application/json",
