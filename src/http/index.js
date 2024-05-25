d91950872dcad6427e46946a3d4d02dcfdc71d7a
JVFA2024 <165958658+JVFA2024@users.noreply.github.com>
2024-05-26 01:33:43 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "https://api-rwqk.onrender.com/",
+  baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
